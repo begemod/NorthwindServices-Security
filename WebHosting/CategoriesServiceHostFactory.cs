b6aec7c9f375b4288ecb397d93c57f38afb4a42c
Roman Arekeev <roman_arekeev@epam.com>
2015-11-16 23:00:06 +0400

    Added host configuration factories to web hosting project.

---
+++ WebHosting/CategoriesServiceHostFactory.cs
@@ -0,0 +1,37 @@
+﻿namespace WebHosting
+{
+    using System;
+    using System.ServiceModel;
+    using System.ServiceModel.Activation;
+    using System.ServiceModel.Description;
+    using WCFServices.Cotracts;
+
+    public class CategoriesServiceHostFactory : ServiceHostFactory
+    {
+        protected override ServiceHost CreateServiceHost(Type serviceType, Uri[] baseAddresses)
+        {
+            var categoriesServiceHost = base.CreateServiceHost(serviceType, baseAddresses);
+
+            categoriesServiceHost.Description.Endpoints.Clear();
+
+            var categoriesServiceNetTcpAddress = new Uri("net.tcp://epruizhw0228:808/NorthwindWCFServices/CategoriesService_2.svc");
+
+            categoriesServiceHost.AddServiceEndpoint(
+                typeof(ICategoriesService),
+                new BasicHttpBinding { TransferMode = TransferMode.Streamed },
+                string.Empty);
+
+            categoriesServiceHost.AddServiceEndpoint(
+                typeof(ICategoriesService),
+                new NetTcpBinding { TransferMode = TransferMode.Streamed },
+                categoriesServiceNetTcpAddress);
+
+            if (categoriesServiceHost.Description.Behaviors.Find<ServiceMetadataBehavior>() == null)
+            {
+                categoriesServiceHost.Description.Behaviors.Add(new ServiceMetadataBehavior { HttpGetEnabled = true });
+            }
+
+            return categoriesServiceHost;
+        }
+    }
+}
