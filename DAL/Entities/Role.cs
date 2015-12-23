d2a152444f5e8b87ee69e734a58fc2fb9340dabc
Roman Arekeev <roman_arekeev@epam.com>
2015-12-23 10:54:42 +0400

    Added new entities.

---
+++ DAL/Entities/Role.cs
@@ -0,0 +1,18 @@
+﻿namespace DAL.Entities
+{
+    using System.Collections.Generic;
+
+    public class Role
+    {
+        public Role()
+        {
+            this.Users = new List<User>();
+        }
+
+        public int RoleId { get; set; }
+
+        public string RoleName { get; set; }
+
+        public ICollection<User> Users { get; set; }
+    }
+}
---
+++ DAL/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿namespace DAL.Entities
+{
+    using System.Collections.Generic;
+
+    public class User
+    {
+        public User()
+        {
+            this.Roles = new List<Role>();
+        }
+
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public ICollection<Role> Roles { get; set; }
+    }
+}
