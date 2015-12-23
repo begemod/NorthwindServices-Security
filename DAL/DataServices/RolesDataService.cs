a2e1cfdb037fb737c093ddb68b7586d60383a313
Roman Arekeev <roman_arekeev@epam.com>
2015-12-23 19:27:02 +0400

    Added new data services.

---
+++ DAL/DataServices/RolesDataService.cs
@@ -0,0 +1,13 @@
+﻿namespace DAL.DataServices
+{
+    using System.Collections.Generic;
+    using DAL.Entities;
+
+    public class RolesDataService
+    {
+        public ICollection<Role> GetUserRoles(int userId)
+        {
+            return new List<Role>();
+        }
+    }
+}
---
+++ DAL/DataServices/UsersDataService.cs
@@ -0,0 +1,17 @@
+﻿namespace DAL.DataServices
+{
+    using DAL.Entities;
+
+    public class UsersDataService
+    {
+        public User GetById(int userId)
+        {
+            return new User();
+        }
+
+        public bool IsExist(User user)
+        {
+            return true;
+        }
+    }
+}
