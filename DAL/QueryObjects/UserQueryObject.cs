e9065f5c51bb605cee4830fe2178d320afcb0427
Roman Arekeev <roman_arekeev@epam.com>
2015-12-23 20:28:31 +0400

    Added UserQueryObject class.

---
+++ DAL/QueryObjects/UserQueryObject.cs
@@ -0,0 +1,19 @@
+﻿namespace DAL.QueryObjects
+{
+    public class UserQueryObject
+    {
+        public QueryObject GetUser(string userName, string password)
+        {
+            const string Sql = @"SELECT
+                                  UserId
+                                 ,UserName
+                                FROM
+                                  dbo.Users
+                                WHERE
+                                  UserName = @UserName
+                                  AND Password = @Password";
+
+            return new QueryObject(Sql, new { UserName = userName, Password = password });
+        }
+    }
+}
