9760f091f7e4eaccb73198d9fc896cda5b0fd0a7
Roman Arekeev <roman_arekeev@epam.com>
2015-12-27 23:07:08 +0400

    Added WCFServices.Infrastructure project.

---
+++ WCFServices.Infrastructure/UserPasswordValidator.cs
@@ -0,0 +1,15 @@
+﻿namespace WCFServices.Infrastructure
+{
+    using System.IdentityModel.Selectors;
+    using System.IdentityModel.Tokens;
+
+    public class UserPasswordValidator : UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            return;
+
+            //throw new SecurityTokenValidationException();
+        }
+    }
+}
