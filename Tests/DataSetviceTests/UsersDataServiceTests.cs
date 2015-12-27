c738f83007c81e3fec8d9ba878d49b5e91956a9e
Roman Arekeev <roman_arekeev@epam.com>
2015-12-27 21:45:31 +0400

    Added test for UsersDataService.

---
+++ Tests/DataSetviceTests/UsersDataServiceTests.cs
@@ -0,0 +1,31 @@
+﻿namespace Tests.DataSetviceTests
+{
+    using System.Linq;
+    using DAL;
+    using DAL.DataServices;
+    using DAL.Infrastructure;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UsersDataServiceTests
+    {
+        private static IConnectionFactory connectionFactory;
+
+        [ClassInitialize]
+        public static void ClassInitialization(TestContext testContext)
+        {
+            connectionFactory = new NortwindDbConnectionFactory();
+        }
+
+        [TestMethod]
+        public void GetGetByUserNameTest()
+        {
+            var usersDataService = new UsersDataService(connectionFactory);
+
+            var user = usersDataService.GetByUserName("Manager1", "Manager1Password");
+
+            Assert.IsNotNull(user);
+            Assert.IsTrue(user.Roles.Any());
+        }
+    }
+}
